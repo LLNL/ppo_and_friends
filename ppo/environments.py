1291c3101555f6cc48b6524e46d1be5827d0afd4
Alister Maguire <alisterowen87@gmail.com>
2021-12-03 09:06:39 -0700

    cartpole from pixels and lunar lander

--- ppo/environments.py
+++ ppo/environments.py
@@ -1,6 +1,100 @@
 from ppo import PPO
 import gym
 from testing import test_policy
+import numpy as np
+
+#FIXME: let's try to remove torch
+import torch
+import torchvision.transforms as t_transforms
+
+class CustomObservationSpace(object):
+
+    def __init__(self,
+                 shape):
+        self.shape = shape
+
+
+class CartPoleEnvManager(object):
+
+    def __init__(self):
+
+        self.env            = gym.make("CartPole-v0").unwrapped
+        self.current_screen = None
+        self.done           = False
+        self.action_space   = self.env.action_space
+
+        self.env.reset()
+        screen_size = self.get_screen_height() * self.get_screen_width() * 3
+        self.observation_space = CustomObservationSpace((screen_size,))
+
+    def reset(self):
+        self.env.reset()
+        self.current_screen = None
+        return self.get_screen_state()
+
+    def close(self):
+        self.env.close()
+
+    def render(self, mode='human'):
+        return self.env.render(mode)
+
+    def num_actions_available(self):
+        return self.env.action_space.n
+
+    def step(self, action):
+        _, reward, self.done, info = self.env.step(action.item())
+        obs = self.get_screen_state()
+        return obs, reward, self.done, info
+
+    def just_starting(self):
+        return self.current_screen is None
+
+    def get_screen_state(self):
+        if self.just_starting() or self.done:
+            self.current_screen = self.get_processed_screen()
+            black_screen = np.zeros_like(self.current_screen)
+            return black_screen.flatten()
+        else:
+            screen_1 = self.current_screen
+            screen_2 = self.get_processed_screen()
+            self.current_screen = screen_2
+            return (screen_2 - screen_1).flatten()
+
+    def get_screen_height(self):
+        return self.get_processed_screen().shape[2]
+
+    def get_screen_width(self):
+        return self.get_processed_screen().shape[3]
+
+    def get_processed_screen(self):
+        screen = self.render("rgb_array").transpose((2, 0, 1))
+        screen = self.crop_screen(screen)
+        return self.transform_screen_data(screen)
+
+    def crop_screen(self, screen):
+
+        screen_height = screen.shape[1]
+        top           = int(screen_height * 0.4)
+        bottom        = int(screen_height * 0.8)
+
+        screen_width  = screen.shape[2]
+        left          = int(screen_width * 0.1)
+        right         = int(screen_width * 0.9)
+        screen        = screen[:, top : bottom, left : right]
+
+        return screen
+
+    def transform_screen_data(self, screen):
+
+        screen = np.ascontiguousarray(screen, dtype=np.float32) / 255.
+        screen = torch.from_numpy(screen)
+
+        resize = t_transforms.Compose([
+            t_transforms.ToPILImage(),
+            t_transforms.Resize((40, 90)),
+            t_transforms.ToTensor()])
+
+        return resize(screen).unsqueeze(0).numpy()
 
 def run_ppo(env,
             action_type,
@@ -23,9 +117,26 @@ def run_ppo(env,
     else: 
         ppo.learn(num_timesteps)
 
+def cartpole_pixels_ppo(state_path,
+                        load_state,
+                        render,
+                        num_timesteps,
+                        device,
+                        test = False):
+
+    env = CartPoleEnvManager()
+
+    run_ppo(env,
+            "discrete",
+            state_path,
+            load_state,
+            render,
+            num_timesteps,
+            device,
+            test)
+
 
 def cartpole_ppo(state_path,
-                 action_type,
                  load_state,
                  render,
                  num_timesteps,
@@ -35,7 +146,7 @@ def cartpole_ppo(state_path,
     env = gym.make('CartPole-v0')
 
     run_ppo(env,
-            action_type,
+            "discrete",
             state_path,
             load_state,
             render,
@@ -44,29 +155,7 @@ def cartpole_ppo(state_path,
             test)
 
 
-def cartpole_pixel_ppo(state_path,
-                       action_type,
-                       load_state,
-                       render,
-                       num_timesteps,
-                       device,
-                       test = False):
-
-    env = gym.make('CartPole-v0')
-
-    #TODO: create a cartpole solver from pixels.
-    #run_ppo(env,
-    #        action_type,
-    #        state_path,
-    #        load_state,
-    #        render,
-    #        num_timesteps,
-    #        device,
-    #        test)
-
-
 def pendulum_ppo(state_path,
-                 action_type,
                  load_state,
                  render,
                  num_timesteps,
@@ -76,7 +165,26 @@ def pendulum_ppo(state_path,
     env = gym.make('Pendulum-v1')
 
     run_ppo(env,
-            action_type,
+            "continuous",
+            state_path,
+            load_state,
+            render,
+            num_timesteps,
+            device,
+            test)
+
+
+def lunar_lander_ppo(state_path,
+                     load_state,
+                     render,
+                     num_timesteps,
+                     device,
+                     test = False):
+
+    env = gym.make('LunarLander-v2')
+
+    run_ppo(env,
+            "discrete",
             state_path,
             load_state,
             render,
--- ppo/main.py
+++ ppo/main.py
@@ -5,9 +5,6 @@ import argparse
 from environments import *
 import os
 
-env_info = {"CartPole" : {"action_type" : "discrete"},
-            "Pendulum" : {"action_type" : "continuous"}}
-
 
 if __name__ == "__main__":
 
@@ -18,12 +15,12 @@ if __name__ == "__main__":
     parser.add_argument("--render", action="store_true")
     parser.add_argument("--num_timesteps", default=500000, type=int)
     parser.add_argument("--environment", type=str, required=True,
-        choices=["CartPole", "Pendulum"])
+        choices=["CartPole", "CartPolePixels", "Pendulum", "LunarLander"])
 
     args          = parser.parse_args()
     test          = args.test
     env_name      = args.environment
-    state_path    = os.path.join(args.state_path, env_name)
+    state_path    = os.path.join(args.state_path, "saved_states", env_name)
     load_state    = args.load_state or test
     render        = args.render
     num_timesteps = args.num_timesteps
@@ -35,18 +32,32 @@ if __name__ == "__main__":
 
     if env_name == "CartPole":
         cartpole_ppo(state_path,
-                     env_info[env_name]["action_type"],
                      load_state,
                      render,
                      num_timesteps,
                      device,
                      test)
 
+    elif env_name == "CartPolePixels":
+        cartpole_pixels_ppo(state_path,
+                            load_state,
+                            render,
+                            num_timesteps,
+                            device,
+                            test)
+
     elif env_name == "Pendulum":
         pendulum_ppo(state_path,
-                     env_info[env_name]["action_type"],
                      load_state,
                      render,
                      num_timesteps,
                      device,
                      test)
+
+    elif env_name == "LunarLander":
+        lunar_lander_ppo(state_path,
+                         load_state,
+                         render,
+                         num_timesteps,
+                         device,
+                         test)
