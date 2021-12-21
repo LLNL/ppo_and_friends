9c95b8c0aa2f447b886f0ed257f21cdbcbeae7d2
Alister Maguire <alisterowen87@gmail.com>
2021-12-20 17:20:31 -0700

    fixes

--- ppo/custom_environments/gym_wrappers.py
+++ ppo/custom_environments/gym_wrappers.py
@@ -2,7 +2,7 @@ from .utils import *
 import numpy as np
 import torch
 import gym
-
+import cv2
 
 class CartPoleEnvManager(object):
 
@@ -89,7 +89,6 @@ class CartPoleEnvManager(object):
 
 
 #FIXME: change to inherit from gym.Wrapper
-#TODO: create a pixel wrapper that stacks frames rather than uses diff.
 class AtariEnvWrapper(object):
 
     def __init__(self,
@@ -97,73 +96,101 @@ class AtariEnvWrapper(object):
                  min_lives = -1,
                  **kwargs):
 
-        super(AtariEnvWrapper, self).__init__()
+        super(AtariEnvWrapper, self).__init__(**kwargs)
 
-        self.min_lives          = min_lives
-        self.env                = env
-        self.action_space       = env.action_space
+        self.min_lives    = min_lives
+        self.env          = env
+        self.action_space = env.action_space
 
     def _end_game(self, done):
         return done or self.env.ale.lives() < self.min_lives
 
 
-class AtariGrayScale(AtariEnvWrapper):
+class AtariPixels(AtariEnvWrapper):
 
     def __init__(self,
                  env,
-                 min_lives = -1):
+                 min_lives  = -1,
+                 frame_size = 84,
+                 **kwargs):
 
-        super(AtariGrayScale, self).__init__(
+        super(AtariPixels, self).__init__(
             env,
-            min_lives)
+            min_lives,
+            **kwargs)
 
-    def rgb_to_cropped_gray_fp(self,
-                               rgb_frame,
-                               h_start = 0,
-                               h_stop  = -1,
-                               w_start = 0,
-                               w_stop  = -1):
+        prev_shape      = env.observation_space.shape
+        self.h_start    = 0
+        self.h_stop     = prev_shape[0]
+        self.w_start    = 0
+        self.w_stop     = prev_shape[1]
 
+        self.frame_size = frame_size
+
+        new_shape    = (1, frame_size, frame_size)
+        self.observation_space = CustomObservationSpace(new_shape)
+
+    def rgb_to_gray(self, rgb_frame):
         rgb_frame  = rgb_frame.astype(np.float32) / 255.
         gray_dot   = np.array([0.2989, 0.587 , 0.114 ], dtype=np.float32)
         gray_frame = np.expand_dims(np.dot(rgb_frame, gray_dot), axis=0)
 
-        return gray_frame[:, h_start : h_stop, w_start : w_stop]
+        return gray_frame
+
+    def crop_frame(self,
+                   frame,
+                   h_start = None,
+                   h_stop  = None,
+                   w_start = None,
+                   w_stop  = None):
+
+        h_start = self.h_start if h_start == None else h_start
+        h_stop  = self.h_stop if h_stop == None else h_stop
+        w_start = self.w_start if w_start == None else w_start
+        w_stop  = self.w_stop if w_stop == None else w_stop
+
+        return frame[:, h_start : h_stop, w_start : w_stop]
+
+    def resize_frame(self,
+                     frame,
+                     shape  = None,
+                     interp = cv2.INTER_AREA):
+
+        #
+        # c2v is reversed.
+        #
+        os_shape = (self.observation_space.shape[2],
+                    self.observation_space.shape[1])
+        shape = os_shape if shape == None else shape
+
+        return cv2.resize(frame, shape, interpolation = interp)
+
+    def rgb_to_processed_frame(self, rgb_frame):
+        new_frame = self.rgb_to_gray(rgb_frame)
+        new_frame = self.crop_frame(new_frame)
+        new_frame = np.expand_dims(self.resize_frame(new_frame.squeeze()), 0)
+        return new_frame
 
 
-class PixelDifferenceEnvWrapper(AtariGrayScale):
+class PixelDifferenceEnvWrapper(AtariPixels):
 
     def __init__(self,
                  env,
-                 min_lives = -1):
-        #
-        # Allow these variables to be over-written by sub-classes.
-        #
-        self.h_start = 0
-        self.h_stop  = -1
-        self.w_start = 0
-        self.w_stop  = -1
+                 min_lives = -1,
+                 **kwargs):
 
         super(PixelDifferenceEnvWrapper, self).__init__(
             env       = env,
-            min_lives = min_lives)
+            min_lives = min_lives,
+            **kwargs)
 
         self.prev_frame   = None
         self.action_space = env.action_space
         self.h_start      = 0
 
-        prev_shape = env.observation_space.shape
-        new_shape  = (1, prev_shape[0], prev_shape[1])
-        self.observation_space = CustomObservationSpace(new_shape)
-
     def reset(self):
         cur_frame = self.env.reset()
-        cur_frame = self.rgb_to_cropped_gray_fp(
-            cur_frame,
-            h_start = self.h_start,
-            h_stop  = self.h_stop,
-            w_start = self.w_start,
-            w_stop  = self.w_stop)
+        cur_frame = self.rgb_to_processed_frame(cur_frame)
 
         self.prev_frame = cur_frame
 
@@ -172,12 +199,7 @@ class PixelDifferenceEnvWrapper(AtariGrayScale):
     def step(self, action):
         cur_frame, reward, done, info = self.env.step(action)
 
-        cur_frame = self.rgb_to_cropped_gray_fp(
-            cur_frame,
-            h_start = self.h_start,
-            h_stop  = self.h_stop,
-            w_start = self.w_start,
-            w_stop  = self.w_stop)
+        cur_frame = self.rgb_to_processed_frame(cur_frame)
 
         diff_frame      = cur_frame - self.prev_frame
         self.prev_frame = cur_frame.copy()
@@ -190,42 +212,26 @@ class PixelDifferenceEnvWrapper(AtariGrayScale):
         self.env.render()
 
 
-class PixelHistEnvWrapper(AtariGrayScale):
+class PixelHistEnvWrapper(AtariPixels):
 
     def __init__(self,
                  env,
                  hist_size = 2,
-                 min_lives = -1):
-
-        prev_shape = env.observation_space.shape
-
-        #
-        # Allow these variables to be over-written by sub-classes.
-        #
-        self.h_start = 0
-        self.h_stop  = prev_shape[0]
-        self.w_start = 0
-        self.w_stop  = prev_shape[1]
+                 min_lives = -1,
+                 **kwargs):
 
         super(PixelHistEnvWrapper, self).__init__(
             env       = env,
-            min_lives = min_lives)
+            min_lives = min_lives,
+            **kwargs)
 
         self.frame_cache  = None
         self.action_space = env.action_space
         self.hist_size    = hist_size
 
-        new_shape  = (hist_size, prev_shape[0], prev_shape[1])
-        self.observation_space = CustomObservationSpace(new_shape)
-
     def reset(self):
         cur_frame = self.env.reset()
-        cur_frame = self.rgb_to_cropped_gray_fp(
-            cur_frame,
-            h_start = self.h_start,
-            h_stop  = self.h_stop,
-            w_start = self.w_start,
-            w_stop  = self.w_stop)
+        cur_frame = self.rgb_to_processed_frame(cur_frame)
 
         self.frame_cache = np.tile(cur_frame, (self.hist_size, 1, 1))
 
@@ -234,14 +240,9 @@ class PixelHistEnvWrapper(AtariGrayScale):
     def step(self, action):
         cur_frame, reward, done, info = self.env.step(action)
 
-        cur_frame = self.rgb_to_cropped_gray_fp(
-            cur_frame,
-            h_start = self.h_start,
-            h_stop  = self.h_stop,
-            w_start = self.w_start,
-            w_stop  = self.w_stop)
+        cur_frame = self.rgb_to_processed_frame(cur_frame)
 
-        #FIXME: remove
+        ##FIXME: remove
         #from PIL import Image
         #import sys
         #foo = (cur_frame.squeeze() * 255.).astype(np.uint8)
@@ -270,7 +271,8 @@ class RAMHistEnvWrapper(AtariEnvWrapper):
 
         super(RAMHistEnvWrapper, self).__init__(
             env       = env,
-            min_lives =  min_lives)
+            min_lives =  min_lives,
+            **kwargs)
 
         ram_shape   = env.observation_space.shape
         cache_shape = (ram_shape[0] * hist_size,)
@@ -358,12 +360,14 @@ class BreakoutRAMEnvWrapper(BreakoutEnvWrapper, RAMHistEnvWrapper):
     def __init__(self,
                  env,
                  hist_size = 2,
-                 min_lives = -1):
+                 min_lives = -1,
+                 **kwargs):
 
         super(BreakoutRAMEnvWrapper, self).__init__(
             env       = env,
             hist_size = hist_size,
-            min_lives = min_lives)
+            min_lives = min_lives,
+            **kwargs)
 
     def step(self, action):
         action = self.action_map[action]
@@ -394,13 +398,15 @@ class BreakoutPixelsEnvWrapper(BreakoutEnvWrapper, PixelHistEnvWrapper):
     def __init__(self,
                  env,
                  hist_size = 2,
-                 min_lives = -1):
+                 min_lives = -1,
+                 **kwargs):
 
 
         super(BreakoutPixelsEnvWrapper, self).__init__(
             env       = env,
             hist_size = hist_size,
-            min_lives = min_lives)
+            min_lives = min_lives,
+            **kwargs)
 
         #
         # Crop the images by removing the "score" information.
@@ -410,7 +416,8 @@ class BreakoutPixelsEnvWrapper(BreakoutEnvWrapper, PixelHistEnvWrapper):
         self.h_stop  = prev_shape[0]
         self.w_start = 0
         self.w_stop  = prev_shape[1]
-        new_shape    = (hist_size, prev_shape[0] - self.h_start, prev_shape[1])
+
+        new_shape    = (hist_size, self.frame_size, self.frame_size)
         self.observation_space = CustomObservationSpace(new_shape)
 
     def step(self, action):
@@ -433,13 +440,7 @@ class BreakoutPixelsEnvWrapper(BreakoutEnvWrapper, PixelHistEnvWrapper):
         cur_frame, _, _, _ = self.env.step(1)
 
 
-        cur_frame = self.rgb_to_cropped_gray_fp(
-            cur_frame,
-            h_start = self.h_start,
-            h_stop  = self.h_stop,
-            w_start = self.w_start,
-            w_stop  = self.w_stop)
-
+        cur_frame = self.rgb_to_processed_frame(cur_frame)
         self.frame_cache = np.tile(cur_frame, (self.hist_size, 1, 1))
 
         return self.frame_cache.copy()
