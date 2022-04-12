3d6ee7cc20c2ed3d7743d874898c554301aa2496
Alister Maguire <alisterowen87@gmail.com>
2022-04-12 10:40:01 -0700

    cleanup

--- environments/env_wrappers.py
+++ environments/env_wrappers.py
@@ -135,6 +135,66 @@ class IdentityWrapper(object):
             load_info(path)
 
 
+class VectorizedEnv(IdentityWrapper):
+    """
+        Wrapper for "vectorizing" environments. As far as I can tell, this
+        definition of vectorize is pretty specific to RL and refers to a way
+        of reducing our environment episodes to "fixed-length trajecorty
+        segments". This allows us to set our maximum timesteps per episode
+        fairly low while still observing an episode from start to finish.
+        This is accomplished by these "segments" that can (and often do) start
+        in the middle of an episode.
+        Vectorized environments are also often referred to as environment
+        wrappers that contain multiple instances of environments, which then
+        return batches of observations, rewards, etc. We don't currently
+        support this second idea.
+    """
+
+    def __init__(self,
+                 env,
+                 **kw_args):
+        """
+            Initialize our vectorized environment.
+
+            Arguments:
+                env    The environment to vectorize.
+        """
+        super(VectorizedEnv, self).__init__(
+            env,
+            **kw_args)
+
+    def step(self, action):
+        """
+            Take a step in our environment with the given action.
+            Since we're vectorized, we reset the environment when
+            we've reached a "done" state.
+
+            Arguments:
+                action    The action to take.
+
+            Returns:
+                The resulting observation, reward, done, and info
+                tuple.
+        """
+        obs, reward, done, info = self.env.step(action)
+
+        #
+        # HACK: some environments are buggy and don't follow their
+        # own rules!
+        #
+        obs = obs.reshape(self.observation_space.shape)
+
+        if type(reward) == np.ndarray:
+            reward = reward[0]
+
+        if done:
+            info["terminal observation"] = obs.copy()
+            obs = self.env.reset()
+            obs = obs.reshape(self.observation_space.shape)
+
+        return obs, reward, done, info
+
+
 class ObservationNormalizer(IdentityWrapper):
     """
         A wrapper for normalizing observations. This normalization
