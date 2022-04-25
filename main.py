b9ccf037bb4deea1320cc6e56e77150218810c1f
Alister Maguire <alisterowen87@gmail.com>
2022-04-25 11:34:02 -0700

    notes

--- main.py
+++ main.py
@@ -49,6 +49,12 @@ if __name__ == "__main__":
     parser.add_argument("--envs_per_proc", default=1, type=int,
         help="The number of environment instances each processor should have.")
 
+    parser.add_argument("--allow_mpi_gpu", action="store_true",
+        help="By default, GPUs are only used if the we're training on a single "
+        "processor, as this is very effecient when using MLPs. This flag will "
+        "allow GPUs to be used with multiple processors, which is useful when "
+        "using networks that are very slow on CPUs (like convolutions).")
+
     parser.add_argument("--force-deterministic", action="store_true",
         help="Tell PyTorch to only use deterministic algorithms.")
 
@@ -89,6 +95,7 @@ if __name__ == "__main__":
     num_timesteps     = args.num_timesteps
     force_determinism = args.force_deterministic
     envs_per_proc     = args.envs_per_proc
+    allow_mpi_gpu     = args.allow_mpi_gpu
 
     if render and render_gif:
         msg  = "ERROR: render and render_gif are both enabled, "
@@ -124,7 +131,10 @@ if __name__ == "__main__":
             shutil.rmtree(state_path)
     comm.barrier()
 
-    if torch.cuda.is_available() and not test and num_procs == 1:
+    if (torch.cuda.is_available() and
+        not test and 
+        (num_procs == 1 or allow_mpi_gpu)):
+
         device = torch.device("cuda")
     else:
         device = torch.device("cpu")
@@ -150,6 +160,10 @@ if __name__ == "__main__":
             test          = test,
             num_test_runs = num_test_runs)
 
+    #
+    # Good distribution for Pendulum:
+    # 2 processors and 2 envs per processor.
+    #
     elif env_name == "Pendulum":
         pendulum_ppo(
             random_seed   = random_seed,
@@ -163,6 +177,10 @@ if __name__ == "__main__":
             test          = test,
             num_test_runs = num_test_runs)
 
+    #
+    # Good distribution for LunarLander:
+    # 2 processors and 2 envs per processor.
+    #
     elif env_name == "LunarLander":
         lunar_lander_ppo(
             random_seed   = random_seed,
@@ -176,6 +194,10 @@ if __name__ == "__main__":
             test          = test,
             num_test_runs = num_test_runs)
 
+    #
+    # Good distribution for LunarLander:
+    # 2 processors and 2 envs per processor.
+    #
     elif env_name == "LunarLanderContinuous":
         lunar_lander_continuous_ppo(
             random_seed   = random_seed,
@@ -191,7 +213,7 @@ if __name__ == "__main__":
 
     #
     # Good distribution for MountainCar:
-    # 1 processor and 3 envs per processor takes about 100 iterations.
+    # 2 processors and 3 envs per processor takes about 6 min.
     #
     elif env_name == "MountainCar":
         mountain_car_ppo(
@@ -206,6 +228,10 @@ if __name__ == "__main__":
             test          = test,
             num_test_runs = num_test_runs)
 
+    #
+    # Good distribution for MountainCarContinuous:
+    # 2 processors and 3 envs per processor.
+    #
     elif env_name == "MountainCarContinuous":
         mountain_car_continuous_ppo(
             random_seed   = random_seed,
@@ -219,6 +245,11 @@ if __name__ == "__main__":
             test          = test,
             num_test_runs = num_test_runs)
 
+    #
+    # Good distribution for Acrobot:
+    # 2 processors and 1 envs per processor learns a good policy
+    # in about 30 seconds to a minute.
+    #
     elif env_name == "Acrobot":
         acrobot_ppo(
             random_seed   = random_seed,
@@ -232,6 +263,14 @@ if __name__ == "__main__":
             test          = test,
             num_test_runs = num_test_runs)
 
+    #
+    # For Atari Pixel environments, I recommend using the --allow_mpi_gpu
+    # so that you can leverage MPI for rollout speed and GPUs for training
+    # convolutional networks. I haven't explored processor and env counts
+    # too much, but 4 processors and 2 envs per proc worked well in
+    # BreakoutPixels. Similar settings worked well for BreakoutRAM, but
+    # you don't need to use the --allow_mpi_gpu flag.
+    #
     elif env_name == "AssaultRAM":
         assault_ram_ppo(
             random_seed   = random_seed,
@@ -284,6 +323,12 @@ if __name__ == "__main__":
             test          = test,
             num_test_runs = num_test_runs)
 
+    #
+    # Good distribution for BipedalWalker:
+    # 4 processors and 1 env per processor will solve the environment
+    # in about 15->20 minutes. The environment is considered solved when
+    # the average score over 100 test runs >= 300.
+    #
     elif env_name == "BipedalWalker":
         bipedal_walker_ppo(
             random_seed   = random_seed,
@@ -297,6 +342,10 @@ if __name__ == "__main__":
             test          = test,
             num_test_runs = num_test_runs)
 
+    # Good distribution for BipedalWalker:
+    # 4 processors and 1 env per processor can solve the environment in
+    # 4 or so hours on my system.
+    #
     elif env_name == "BipedalWalkerHardcore":
         bipedal_walker_hardcore_ppo(
             random_seed   = random_seed,
@@ -310,6 +359,11 @@ if __name__ == "__main__":
             test          = test,
             num_test_runs = num_test_runs)
 
+    #
+    # Good distribution for InvertedPendulum:
+    # 2 processors and 2 envs per processor learns a good policy
+    # in about 10 seconds of training.
+    #
     elif env_name == "InvertedPendulum":
         inverted_pendulum_ppo(
             random_seed   = random_seed,
@@ -323,6 +377,11 @@ if __name__ == "__main__":
             test          = test,
             num_test_runs = num_test_runs)
 
+    #
+    # Good distribution for InvertedPendulum:
+    # 2 processors and 2 envs per processor learns a good policy
+    # within a few minutes.
+    #
     elif env_name == "InvertedDoublePendulum":
         inverted_double_pendulum_ppo(
             random_seed   = random_seed,
@@ -336,6 +395,11 @@ if __name__ == "__main__":
             test          = test,
             num_test_runs = num_test_runs)
 
+    #
+    # Good distribution for Ant:
+    # 2 processors and 2 envs per processor learns an excellent
+    # policy in ~10 min.
+    #
     elif env_name == "Ant":
         ant_ppo(
             random_seed   = random_seed,
@@ -349,6 +413,10 @@ if __name__ == "__main__":
             test          = test,
             num_test_runs = num_test_runs)
 
+    #
+    # Good distribution for Walker:
+    # 2 processors and 2 envs per processor works well.
+    #
     elif env_name == "Walker2d":
         walker2d_ppo(
             random_seed   = random_seed,
@@ -362,6 +430,14 @@ if __name__ == "__main__":
             test          = test,
             num_test_runs = num_test_runs)
 
+    #
+    # Good distribution for Hopper:
+    # Hopper is oddly sensitive to the trajectory lengths. It will learn
+    # to go a bit beyond your max trajectory length very well, but will
+    # often fall after that. The max ts per rollout are set to 2048 in
+    # this environment, so I recommend using 2 processors or 2 environments
+    # per proc but not both.
+    #
     elif env_name == "Hopper":
         hopper_ppo(
             random_seed   = random_seed,
@@ -375,6 +451,10 @@ if __name__ == "__main__":
             test          = test,
             num_test_runs = num_test_runs)
 
+    #
+    # Good distribution for Swimmer:
+    # 2 processors and 2 envs per processor works well.
+    #
     elif env_name == "Swimmer":
         swimmer_ppo(
             random_seed   = random_seed,
@@ -388,6 +468,11 @@ if __name__ == "__main__":
             test          = test,
             num_test_runs = num_test_runs)
 
+    #
+    # Good distribution for HalfCheetah:
+    # 2 processors and 2 envs per processor will learn an excellent
+    # policy in about 2 minutes.
+    #
     elif env_name == "HalfCheetah":
         half_cheetah_ppo(
             random_seed   = random_seed,
@@ -401,6 +486,11 @@ if __name__ == "__main__":
             test          = test,
             num_test_runs = num_test_runs)
 
+    #
+    # Good distribution for Humanoid:
+    # 2 processors and 2 envs per processor learns an excellent policy in
+    # 40 minutes or less.
+    #
     elif env_name == "Humanoid":
         humanoid_ppo(
             random_seed   = random_seed,
