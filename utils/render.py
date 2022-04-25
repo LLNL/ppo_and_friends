81972772c443d96a6c89ff339ba90eca57c2d9aa
Alister Maguire <alisterowen87@gmail.com>
2022-04-25 14:57:51 -0700

    cut frame option

--- utils/render.py
+++ utils/render.py
@@ -1,21 +1,31 @@
 from matplotlib import animation
 import matplotlib.pyplot as plt
 from moviepy.editor import ImageSequenceClip
+import numpy as np
 import os
 
 def save_frames_as_gif(
     frames,
     out_path,
-    filename = 'test.gif',
-    fps      = 50):
+    filename     = 'test.gif',
+    fps          = 15,
+    frame_reduce = 2):
     """
         Save numpy frames as a gif.
 
         Arguments:
-            frames       A list of rendered frames as rgb arrays.
-            out_path     The path to save the gif to.
-            filename     The name of the gif.
-            fps          Frames per second.
+            frames        A list of rendered frames as rgb arrays.
+            out_path      The path to save the gif to.
+            filename      The name of the gif.
+            fps           Frames per second.
+            frame_reduce  Reduce frames 2^frame_reduce. This can help with speed
+                          when displaying gifs in a browser.
     """
+    #
+    # Cut the frames in half for for every frame_reduce.
+    #
+    for i in range(frame_reduce):
+        frames = np.array(frames)[1::2]
+
     clip = ImageSequenceClip(list(frames), fps=fps)
     clip.write_gif(os.path.join(out_path, filename), fps=fps)
