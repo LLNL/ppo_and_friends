76e0a405adcec01c4502ef563df1f0692ac739df
Alister Maguire <maguire7@llnl.gov>
2022-08-02 08:47:20 -0700

    option for ts_per_agent

--- ppo.py
+++ ppo.py
@@ -38,6 +38,7 @@ class PPO(object):
                  device,
                  random_seed,
                  is_multi_agent      = False,
+                 apply_ts_per_agent  = True,
                  add_agent_ids       = False,
                  envs_per_proc       = 1,
                  icm_network         = ICM,
@@ -92,6 +93,9 @@ class PPO(object):
                  random_seed          A random seed to use.
                  is_multi_agent       Does our environment contain multiple
                                       agents? If so, enable MAPPO algorithm.
+                 apply_ts_per_agent   If True, ts_per_rollout will be the cap
+                                      for each agent. This results in
+                                      ts_per_rollout *= number_of_agents.
                  add_agent_ids        Optionally add agent ids to the
                                       observations. This is only valid when
                                       is_multi_agent == True.
@@ -330,7 +334,8 @@ class PPO(object):
         # want when num_agents > 1.
         #
         if is_multi_agent:
-            ts_per_rollout *= env.get_num_agents()
+            if apply_ts_per_agent:
+                ts_per_rollout *= env.get_num_agents()
             self.num_agents = env.get_num_agents()
         else:
             self.num_agents = 1
