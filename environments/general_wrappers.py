19121b8acf1b50284bbcbede6e0334d316e53aa3
alisterowen <alisterowen87@gmail.com>
2022-11-15 11:37:06 -0700

    first pass ma gym wrapper

--- environments/general_wrappers.py
+++ environments/general_wrappers.py
@@ -575,13 +575,142 @@ class SingleAgentGymWrapper(PPOEnvironmentWrapper):
         obs, global_obs = self._wrap_gym_reset(self.env.reset())
         return obs, global_obs
 
-    #FIXME: cleanup
-    #def soft_reset(self):
-    #    """
-    #    """
-    #    if self.need_hard_reset:
-    #        return self.reset()
-    #    return self.obs_cache, self.obs_cache
+    def seed(self, seed):
+        """
+        """
+        self.env.seed(seed)
+
+class MultiAgentGymWrapper(PPOEnvironmentWrapper):
+
+    def __init__(self,
+                 env,
+                 test_mode = False,
+                 **kw_args):
+        """
+        """
+        num_agents     = len(env.observation_space)
+        self.agent_ids = {f"agent{i}" for i in range(num_agents)}
+
+        super(MultiAgentGymWrapper, self).__init__(
+            env,
+            test_mode,
+            **kw_args)
+
+        if self.add_agent_ids:
+            msg  = "WARNING: adding agent ids is not applicable "
+            msg += "for single agent simulators. Disregarding."
+            rank_print(msg)
+
+        #
+        # Environments are very inconsistent! Some of them require their
+        # actions to be squeezed before they can be sent to the env.
+        #
+        self.action_squeeze = need_action_squeeze(self.env)
+
+    def define_multi_agent_spaces(self):
+        """
+        """
+        self.action_space             = Dict()
+        self.observation_space        = Dict()
+        self.critic_observation_space = Dict()
+
+        for a_idx, a_id in enumerate(self.agent_ids):
+            self.action_space[a_id]             = self.env.action_space[a_idx]
+            self.observation_space[a_id]        = self.env.observation_space[a_idx]
+            self.critic_observation_space[a_id] = self.env.observation_space[a_idx]
+
+    def _unwrap_action(self,
+                       actions):
+        """
+        """
+        gym_actions = np.array([None] * self.num_agents)
+
+        for a_idx, a_id in enumerate(self.agent_ids):
+            env_action = actions[a_id]
+
+            if self.action_squeeze:
+                env_action = env_action.squeeze()
+
+            gym_actions[a_idx] = env_action
+
+        return Tuple(gym_actions)
+
+    def _wrap_gym_step(self,
+                       obs,
+                       reward,
+                       done,
+                       info):
+        """
+        """
+        wrapped_obs    = {}
+        wrapped_reward = {}
+        wrapped_done   = {}
+        wrapped_info   = {}
+
+        for a_idx, a_id in enumerate(self.agent_ids):
+            agent_obs    = obs[a_idx]
+            agent_reward = reward[a_idx]
+            agent_done   = agent_done[a_idx]
+            agent_info   = info
+
+            #
+            # HACK: some environments are buggy and don't follow their
+            # own rules!
+            #
+            agent_obs = agent_obs.reshape(self.observation_space[a_id].shape)
+
+            if type(agent_reward) == np.ndarray:
+                agent_reward = agent_reward[0]
+
+            agent_reward = np.float32(agent_reward)
+
+            wrapped_obs[a_id]    = agent_obs
+            wrapped_reward[a_id] = agent_reward
+            wrapped_info[a_id]   = agent_info
+            wrapped_don[a_id]    = agent_done
+
+        if done.all():
+            self.all_done = True
+
+        return (wrapped_obs, deepcopy(wrapped_obs),
+            wrapped_reward, wrapped_done, wrapped_info)
+
+    def _wrap_gym_reset(self,
+                        obs):
+        """
+        """
+        wrapped_obs = {}
+
+        for a_id, a_idx in enumerate(self.agent_ids):
+            agent_obs = obs[a_idx]
+
+            #
+            # HACK: some environments are buggy and don't follow their
+            # own rules!
+            #
+            agent_obs = agent_obs.reshape(self.observation_space[a_id].shape)
+            wrapped_obs[a_id] = agent_obs
+
+        return wrapped_obs, deepcopy(wrapped_obs)
+
+    #FIXME: this can go into an abstract parent class.
+    def step(self, actions):
+        """
+        """
+        obs, global_obs, reward, done, info = \
+            self._wrap_gym_step(*self.env.step(
+                self._unwrap_action(actions)))
+
+        self.obs_cache = deepcopy(obs)
+        self.need_hard_reset = False
+
+        return obs, global_obs, reward, done, info
+
+    def reset(self):
+        """
+        """
+        obs, global_obs = self._wrap_gym_reset(self.env.reset())
+        return obs, global_obs
 
     def seed(self, seed):
         """
