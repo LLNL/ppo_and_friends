04c0e4fbd74edd9694c6ff7084d3c798306af1a2
alisterowen <alisterowen87@gmail.com>
2023-03-17 14:11:29 -0700

    mpi

--- test/tests/train/test_cart_pole.py
+++ test/tests/train/test_cart_pole.py
@@ -1,13 +1,22 @@
 from utils import extrinsic_score_test
 
-def test_cart_pole_train():
+def test_cart_pole():
 
     num_timesteps = 17000
     cmd  = f"python train_baseline.py "
     cmd += f"-e CartPole --clobber --num-timesteps {num_timesteps}"
 
-    extrinsic_score_test("cart-pole-train", cmd)
+    extrinsic_score_test("cart-pole", cmd)
+
+def test_cart_pole_mpi():
+
+    num_timesteps = 40000
+    cmd  = f"mpirun -n 4 python train_baseline.py "
+    cmd += f"-e CartPole --clobber --num-timesteps {num_timesteps}"
+
+    extrinsic_score_test("cart-pole-mpi", cmd)
 
 
 if __name__ == "__main__":
-    test_cart_pole_train()
+    test_cart_pole()
+    test_cart_pole_mpi()
