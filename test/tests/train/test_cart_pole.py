541867bdddfd8585f07412f44e2aa614c7fbf5cc
alisterowen <alisterowen87@gmail.com>
2023-03-23 14:26:12 -0700

    more testing

--- test/tests/train/test_cart_pole.py
+++ test/tests/train/test_cart_pole.py
@@ -1,12 +1,12 @@
-from utils import extrinsic_score_test
+from utils import extrinsic_score_training_test
 
-def test_cart_pole():
+def test_cart_pole_serial():
 
     num_timesteps = 17000
     cmd  = f"python train_baseline.py "
     cmd += f"-e CartPole --clobber --num-timesteps {num_timesteps}"
 
-    extrinsic_score_test("cart-pole", cmd, 200.)
+    extrinsic_score_training_test("cart-pole-serial", cmd, 200.)
 
 def test_cart_pole_mpi():
 
@@ -14,7 +14,7 @@ def test_cart_pole_mpi():
     cmd  = f"mpirun -n 4 python train_baseline.py "
     cmd += f"-e CartPole --clobber --num-timesteps {num_timesteps}"
 
-    extrinsic_score_test("cart-pole-mpi", cmd, 200.)
+    extrinsic_score_training_test("cart-pole-mpi", cmd, 200.)
 
 def test_cart_pole_multi_envs():
 
@@ -23,7 +23,7 @@ def test_cart_pole_multi_envs():
     cmd += f"-e CartPole --clobber --num-timesteps {num_timesteps} "
     cmd += f"--envs-per-proc 2"
 
-    extrinsic_score_test("cart-pole-mpi", cmd, 128.)
+    extrinsic_score_training_test("cart-pole-multi-env", cmd, 128.)
 
 def test_cart_pole_multi_envs_mpi():
 
@@ -32,11 +32,20 @@ def test_cart_pole_multi_envs_mpi():
     cmd += f"-e CartPole --clobber --num-timesteps {num_timesteps} "
     cmd += f"--envs-per-proc 2"
 
-    extrinsic_score_test("cart-pole-mpi", cmd, 128.)
+    extrinsic_score_training_test("cart-pole-multi-env-mpi", cmd, 128.)
+
+def test_binary_cart_pole_serial():
+
+    num_timesteps = 17000
+    cmd  = f"python train_baseline.py "
+    cmd += f"-e BinaryCartPole --clobber --num-timesteps {num_timesteps}"
+
+    extrinsic_score_training_test("binary-cart-pole-serial", cmd, 200.)
 
 
 if __name__ == "__main__":
-    test_cart_pole()
+    test_cart_pole_serial()
     test_cart_pole_mpi()
     test_cart_pole_multi_envs()
     test_cart_pole_multi_envs_mpi()
+    test_binary_cart_pole_serial()
--- test/tests/train/utils.py
+++ test/tests/train/utils.py
@@ -1,4 +1,5 @@
 import sys
+import dill as pickle
 import subprocess
 import os
 from pathlib import Path
@@ -49,10 +50,13 @@ def get_final_extrinsic_score_avg(stdout):
         score = None
     return score
 
-def extrinsic_score_test(name, train_command, passing_score):
+def extrinsic_score_training_test(name, train_command, passing_score):
 
     root_dir = get_root_dir()
     cur_dir  = Path(os.getcwd())
+
+    train_command += f" --state-path {cur_dir}"
+    print(f"Running command: {train_command}")
 
     os.chdir(root_dir)
     result = subprocess.run(train_command.split(),
@@ -73,3 +77,33 @@ def extrinsic_score_test(name, train_command, passing_score):
     assert score_avg >= passing_score, fail_msg
 
     print(f"\n************{name} PASSED************")
+
+def average_test_score_test(name, test_command, passing_scores, state_dir):
+
+    root_dir = get_root_dir()
+    cur_dir  = Path(os.getcwd())
+
+    test_command += f" --state-path {cur_dir}"
+    print(f"Running command: {test_command}")
+
+    os.chdir(root_dir)
+    result = subprocess.run(test_command.split(),
+        capture_output=True, text=True)
+    os.chdir(cur_dir)
+
+    score_file = os.path.join("saved_states", state_dir, "test-scores.pickle")
+    with open(score_file, "rb") as in_f:
+        scores = pickle.load(in_f)
+
+    fail_msg  = f"\n************{name} FAILED************"
+    fail_msg += f"\nExpected avg scores:\n {passing_scores}"
+    fail_msg += f"\nActual scores:\n {scores}"
+
+    for agent_id in passing_scores:
+        msg = f"ERROR: unable to find agent {agent_id} in the test scores."
+        assert agent_id in scores, msg
+
+        assert scores[agent_id]["avg_score"] >= passing_scores[agent_id], fail_msg
+
+    print(f"\n************{name} PASSED************")
+
