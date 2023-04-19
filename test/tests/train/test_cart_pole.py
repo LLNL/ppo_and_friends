b278d64c3ffacf0cc771190d6e702aecfbb0f6a0
alisterowen <alisterowen87@gmail.com>
2023-04-19 11:25:10 -0700

    uncomment

--- test/tests/train/test_cart_pole.py
+++ test/tests/train/test_cart_pole.py
@@ -12,57 +12,57 @@ def run_cart_pole_test(name, num_test_runs=10):
     average_score_test(name, cmd,
         passing_scores, "CartPole")
 
-#def test_cart_pole_serial():
-#
-#    num_timesteps = 70000
-#    cmd  = f"ppoaf-baselines "
-#    cmd += f"CartPole --clobber --num-timesteps {num_timesteps}"
-#
-#    run_training(cmd)
-#    run_cart_pole_test("cart-pole-serial")
-#
-#def test_cart_pole_mpi():
-#
-#    num_timesteps = 70000
-#    cmd  = f"mpirun -n 2 ppoaf-baselines "
-#    cmd += f"CartPole --clobber --num-timesteps {num_timesteps}"
-#
-#    run_training(cmd)
-#    run_cart_pole_test("cart-pole-mpi")
-#
-#def test_cart_pole_multi_envs():
-#
-#    num_timesteps = 90000
-#    cmd  = f"ppoaf-baselines "
-#    cmd += f"CartPole --clobber --num-timesteps {num_timesteps} "
-#    cmd += f"--envs-per-proc 2"
-#
-#    run_training(cmd)
-#    run_cart_pole_test("cart-pole-multi-env")
-#
-#def test_cart_pole_multi_envs_mpi():
-#
-#    num_timesteps = 90000
-#    cmd  = f"mpirun -n 2 ppoaf-baselines "
-#    cmd += f"CartPole --clobber --num-timesteps {num_timesteps} "
-#    cmd += f"--envs-per-proc 2"
-#
-#    run_training(cmd)
-#    run_cart_pole_test("cart-pole-multi-env-mpi")
-#
-#def test_binary_cart_pole_serial():
-#
-#    num_timesteps = 70000
-#    cmd  = f"ppoaf-baselines "
-#    cmd += f"BinaryCartPole --clobber --num-timesteps {num_timesteps}"
-#
-#    run_training(cmd)
-#    run_cart_pole_test("cart-pole-binary-serial")
-#
-#
-#if __name__ == "__main__":
-#    test_cart_pole_serial()
-#    test_cart_pole_mpi()
-#    test_cart_pole_multi_envs()
-#    test_cart_pole_multi_envs_mpi()
-#    test_binary_cart_pole_serial()
+def test_cart_pole_serial():
+
+    num_timesteps = 70000
+    cmd  = f"ppoaf-baselines "
+    cmd += f"CartPole --clobber --num-timesteps {num_timesteps}"
+
+    run_training(cmd)
+    run_cart_pole_test("cart-pole-serial")
+
+def test_cart_pole_mpi():
+
+    num_timesteps = 70000
+    cmd  = f"mpirun -n 2 ppoaf-baselines "
+    cmd += f"CartPole --clobber --num-timesteps {num_timesteps}"
+
+    run_training(cmd)
+    run_cart_pole_test("cart-pole-mpi")
+
+def test_cart_pole_multi_envs():
+
+    num_timesteps = 90000
+    cmd  = f"ppoaf-baselines "
+    cmd += f"CartPole --clobber --num-timesteps {num_timesteps} "
+    cmd += f"--envs-per-proc 2"
+
+    run_training(cmd)
+    run_cart_pole_test("cart-pole-multi-env")
+
+def test_cart_pole_multi_envs_mpi():
+
+    num_timesteps = 90000
+    cmd  = f"mpirun -n 2 ppoaf-baselines "
+    cmd += f"CartPole --clobber --num-timesteps {num_timesteps} "
+    cmd += f"--envs-per-proc 2"
+
+    run_training(cmd)
+    run_cart_pole_test("cart-pole-multi-env-mpi")
+
+def test_binary_cart_pole_serial():
+
+    num_timesteps = 70000
+    cmd  = f"ppoaf-baselines "
+    cmd += f"BinaryCartPole --clobber --num-timesteps {num_timesteps}"
+
+    run_training(cmd)
+    run_cart_pole_test("cart-pole-binary-serial")
+
+
+if __name__ == "__main__":
+    test_cart_pole_serial()
+    test_cart_pole_mpi()
+    test_cart_pole_multi_envs()
+    test_cart_pole_multi_envs_mpi()
+    test_binary_cart_pole_serial()
