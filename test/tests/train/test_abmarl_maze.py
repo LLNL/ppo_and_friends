f16bd6993f2ca62fd68097b178e9ec610240eaae
alisterowen <alisterowen87@gmail.com>
2023-04-19 08:49:01 -0700

    remove abmarl

--- test/tests/train/test_abmarl_maze.py
+++ test/tests/train/test_abmarl_maze.py
@@ -1,20 +1,23 @@
 from utils import run_training, average_score_test
 
-def test_abmarl_blind_maze_mpi():
+# FIXME: Abmarl currently relies on an old version of Gym, and this
+# is causing issues with pip in Github's CI.
 
-    num_timesteps = 85000
-    cmd  = f"mpirun -n 2 ppoaf-baselines "
-    cmd += f"AbmarlBlindMaze --clobber --num-timesteps {num_timesteps}"
-
-    run_training(cmd)
-
-    cmd  = f"ppoaf-baselines "
-    cmd += f"AbmarlBlindMaze --test --num-test-runs 5 "
-
-    passing_scores = {"navigator" : 0.8}
-
-    average_score_test("abmarl-blind-maze-mpi", cmd,
-        passing_scores, "AbmarlBlindMaze")
-
-if __name__ == "__main__":
-    test_abmarl_blind_maze_mpi()
+#def test_abmarl_blind_maze_mpi():
+#
+#    num_timesteps = 85000
+#    cmd  = f"mpirun -n 2 ppoaf-baselines "
+#    cmd += f"AbmarlBlindMaze --clobber --num-timesteps {num_timesteps}"
+#
+#    run_training(cmd)
+#
+#    cmd  = f"ppoaf-baselines "
+#    cmd += f"AbmarlBlindMaze --test --num-test-runs 5 "
+#
+#    passing_scores = {"navigator" : 0.8}
+#
+#    average_score_test("abmarl-blind-maze-mpi", cmd,
+#        passing_scores, "AbmarlBlindMaze")
+#
+#if __name__ == "__main__":
+#    test_abmarl_blind_maze_mpi()
