824155d64d0549cecb3ff4761c06bcca22738119
alisterowen <alisterowen87@gmail.com>
2023-05-23 13:11:05 -0700

    space support for abmarl

--- environments/gym/version_wrappers.py
+++ environments/gym/version_wrappers.py
@@ -61,3 +61,62 @@ class Gym21To26():
             Render the environment.
         """
         return self.env.render(*args, **kw_args)
+
+
+def gym_space_to_gymnasium_space(space):
+    """
+        gym and gymnasium spaces are incompatible. This function
+        just converts gym spaces to gymnasium spaces to bypass
+        the errors that crop up.
+
+        Arguments:
+            space     The gym space to convert.
+
+        Returns:
+            The input space converted to gymnasium.
+    """
+    import gym as old_gym
+    if type(space) == old_gym.spaces.Box:
+        space = gym.spaces.Box(
+            low   = space.low,
+            high  = space.high,
+            shape = space.shape,
+            dtype = space.dtype)
+
+    elif type(space) == old_gym.spaces.Discrete:
+        space = gym.spaces.Discrete(
+            n     = space.n,
+            start = space.start)
+
+    elif type(space) == old_gym.spaces.MultiBinary:
+        space = gym.spaces.MultiBinary(
+            n     = space.n)
+
+    elif type(space) == old_gym.spaces.MultiDiscrete:
+        space = gym.spaces.MultiDiscrete(
+            nvec  = space.nvec,
+            dtype = space.dtype)
+
+    elif type(space) == old_gym.spaces.Dict:
+        new_space = gym.spaces.Dict()
+
+        for key in space:
+            new_space[key] = gym_space_to_gymnasium_space(space[key])
+
+        space = new_space
+
+    elif type(space) == old_gym.spaces.Tuple:
+        new_space = []
+
+        for subspace in space:
+            new_space.append(gym_space_to_gymnasium_space(subspace))
+
+        space = gym.spaces.Tuple(new_space)
+
+    else:
+        msg  = "WARNING: skipping conversion of space "
+        msg += f"{type(self.action_space[agent_id])}."
+        print(msg)
+
+    return space
+
