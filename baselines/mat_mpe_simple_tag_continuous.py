17e2bbcebb6c62de99fa63ab720625d0106448fa
Alister Maguire <maguire7@llnl.gov>
2023-08-31 15:30:07 -0700

    notes

--- baselines/mat_mpe_simple_tag_continuous.py
+++ baselines/mat_mpe_simple_tag_continuous.py
@@ -35,12 +35,6 @@ class MATMPESimpleTagContinuousRunner(GymRunner):
         adversary_kw_args  = {}
         adversary_kw_args["distribution_min"] = 0.0
         adversary_kw_args["distribution_max"] = 1.0
-        #adversary_kw_args["activation"] = nn.Tanh()
-        #adversary_kw_args["decoder_activation"] = nn.Tanh()
-        #adversary_kw_args["encoder_activation"] = nn.Tanh()
-        #adversary_kw_args["internal_init"] = nn.init.calculate_gain('tanh')
-        #adversary_kw_args["encoder_internal_init"] = nn.init.calculate_gain('tanh')
-        #adversary_kw_args["decoder_internal_init"] = nn.init.calculate_gain('tanh')
 
         lr = 0.0003
 
