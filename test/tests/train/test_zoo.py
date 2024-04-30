9cd27761598406f8c3da43464c7610ba83a229ef
Alister Maguire <maguire7@llnl.gov>
2024-04-30 15:09:38 -0700

    new test

--- test/tests/train/test_zoo.py
+++ test/tests/train/test_zoo.py
@@ -43,3 +43,21 @@ def test_mat_mpe_simple_tag_continuous_mpi(num_ranks):
 
     high_score_test('mat mpi mpe simple tag continuous',
         'mpe_simple_tag.py', 10, passing_scores)
+
+def test_agent_shared_icm(num_ranks):
+    #
+    # I don't care about the scores here. I merely want to make
+    # sure the agent shared ICM setting doesn't cause any crashes.
+    #
+    num_timesteps = 10000
+    passing_scores = {"adversary" : 0.0}
+
+    run_training(
+        baseline_type   = 'pettingzoo',
+        baseline_runner = 'mpe_simple_adversary.py',
+        options         = '--policy mat --icm 1',
+        num_timesteps   = num_timesteps,
+        num_ranks       = num_ranks)
+
+    high_score_test('agent shared ICM',
+        'mpe_simple_adversary.py', 1, passing_scores)
