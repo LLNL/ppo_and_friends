71339ff8d8bfc3ee61e54b071edf5406b7bccec4
Alister Maguire <maguire7@llnl.gov>
2024-05-01 14:46:35 -0700

    one mc to rule them all

--- baselines/gymnasium/mountain_car.py
+++ baselines/gymnasium/mountain_car.py
@@ -26,32 +26,40 @@ class MountainCarRunner(GymRunner):
         """
         parser.add_argument("--bs_clip_min", default=-np.inf, type=float)
         parser.add_argument("--bs_clip_max", default=np.inf, type=float)
-        parser.add_argument("--cut_off_bs_clip", default=0, type=int)
+
+        parser.add_argument("--continuous_actions", default=0, choices=[0,1], type=int)
 
         parser.add_argument("--learning_rate", default=0.0003, type=float)
+        parser.add_argument("--actor_hidden_size", default=32, type=int)
+        parser.add_argument("--critic_hidden_size", default=64, type=int)
 
-        parser.add_argument("--enable_icm", type=int, default=0)
+        parser.add_argument("--enable_icm", type=int, default=0, choices=[0,1])
         parser.add_argument("--icm_inverse_size", type=int, default=32)
         parser.add_argument("--icm_inverse_depth", type=int, default=2)
         parser.add_argument("--icm_forward_size", type=int, default=32)
         parser.add_argument("--icm_forward_depth", type=int, default=2)
-        parser.add_argument("--icm_encoded_obs_dim", type=int, default=0)
-        parser.add_argument("--icm_learning_rate", type=float, default=0.0003)
-        parser.add_argument("--intr_reward_weight", type=float, default=1.0)
+        parser.add_argument("--icm_encoded_obs_dim", type=int, default=2)
+        parser.add_argument("--icm_learning_rate", type=float, default=0.001)
+        parser.add_argument("--intr_reward_weight", type=float, default=50.0)
         return parser
 
     def run(self):
 
+        if bool(self.cli_args.continuous_actions):
+            env_name = 'MountainCarContinuous-v0'
+        else:
+            env_name = 'MountainCar-v0'
+
         env_generator = lambda : \
-            SingleAgentGymWrapper(gym.make('MountainCar-v0',
+            SingleAgentGymWrapper(gym.make(env_name,
                 render_mode = self.get_gym_render_mode()))
 
         actor_kw_args = {"activation" : nn.LeakyReLU()}
-        actor_kw_args["hidden_size"]  = 32
+        actor_kw_args["hidden_size"]  = self.cli_args.actor_hidden_size
         actor_kw_args["hidden_depth"] = 2
 
         critic_kw_args = actor_kw_args.copy()
-        critic_kw_args["hidden_size"]  = 64
+        critic_kw_args["hidden_size"]  = self.cli_args.critic_hidden_size
         critic_kw_args["hidden_depth"] = 2
 
         icm_kw_args = {}
@@ -61,25 +69,8 @@ class MountainCarRunner(GymRunner):
         icm_kw_args["forward_hidden_depth"] = self.cli_args.icm_forward_depth
         icm_kw_args["forward_hidden_size"]  = self.cli_args.icm_forward_size
 
-        if self.cli_args.cut_off_bs_clip > 0:
-            bs_clip_min  = LinearStepScheduler(
-                status_key      = "extrinsic reward avg",
-                status_preface  = "single_agent",
-                initial_value   = self.cli_args.bs_clip_min,
-                compare_fn      = np.greater_equal,
-                status_triggers = [-140,],
-                step_values     = [-np.inf,])
-
-            bs_clip_max  = LinearStepScheduler(
-                status_key      = "extrinsic reward avg",
-                status_preface  = "single_agent",
-                initial_value   = self.cli_args.bs_clip_max,
-                compare_fn      = np.greater_equal,
-                status_triggers = [-140,],
-                step_values     = [np.inf,])
-        else:
-            bs_clip_min = self.cli_args.bs_clip_min
-            bs_clip_max = self.cli_args.bs_clip_max
+        bs_clip_min = self.cli_args.bs_clip_min
+        bs_clip_max = self.cli_args.bs_clip_max
 
         policy_args = {\
             "ac_network"         : FeedForwardNetwork,
@@ -105,23 +96,13 @@ class MountainCarRunner(GymRunner):
 
         ts_per_rollout = self.get_adjusted_ts_per_rollout(200)
 
-        #
-        # NOTE: This environment performs dramatically  better when
-        # max_ts_per_ep is set to the total timesteps allowed by the
-        # environment. It's not 100% clear to me why this is the case.
-        # We should probably explore this a bit more. MountainCarContinuous
-        # doesn't seem to exhibit this behavior, so it's unlikely an issue
-        # with ICM.
-        # Also, the extrinsic reward weight fraction is very important
-        # for good performance.
-        #
         self.run_ppo(env_generator      = env_generator,
                      save_when          = save_when,
+                     ts_per_rollout     = ts_per_rollout,
                      policy_settings    = policy_settings,
                      policy_mapping_fn  = policy_mapping_fn,
-                     ts_per_rollout     = ts_per_rollout,
+                     max_ts_per_ep      = 128,
                      epochs_per_iter    = 32,
-                     max_ts_per_ep      = 200,
                      normalize_obs      = False,
                      normalize_rewards  = False,
                      normalize_values   = False,
