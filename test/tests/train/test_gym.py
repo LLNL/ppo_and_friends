45ec9324c00c50b8ee9b5280402bad99d5204317
Alister Maguire <maguire7@llnl.gov>
2024-08-23 11:49:12 -0700

    fixes

--- test/tests/train/test_gym.py
+++ test/tests/train/test_gym.py
@@ -2,7 +2,7 @@ from utils import run_training, high_score_test
 
 def test_mat_robot_warehouse_tiny(num_ranks):
     num_timesteps = 10000000
-    passing_scores = {"single_agent" : 3.0}
+    passing_scores = {"rware" : 3.0}
 
     run_training(
         baseline_type   = 'gym',
